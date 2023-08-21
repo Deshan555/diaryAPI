880525be303c16db0e589cdd7dcd842531801df7
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-08-21 20:01:02 +0530

    reformat the data repository code

--- src/main/java/com/weblog/diary/repo/DayBookRepo.java
+++ src/main/java/com/weblog/diary/repo/DayBookRepo.java
@@ -3,6 +3,11 @@ package com.weblog.diary.repo;
 import com.weblog.diary.model.DayBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DayBookRepo extends JpaRepository<DayBook, Long> {
+import java.util.Optional;
 
+public interface DayBookRepo extends JpaRepository<DayBook, Long> {
+    // query methods
+    void deleteDayBookById(Long id);
+
+    Optional<DayBook> findDayBookById(Long id);
 }

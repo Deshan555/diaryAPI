99c1fda0af2acb07c7d18606d01324a6d8ca2a25
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-08-21 20:05:14 +0530

    reformat the DaybookService.java and implement the all related methods
    add auto timestamping machinist and auto UUID macadamising

--- src/main/java/com/weblog/diary/service/DaybookService.java
+++ src/main/java/com/weblog/diary/service/DaybookService.java
@@ -1,4 +1,45 @@
 package com.weblog.diary.service;
 
+import com.weblog.diary.exception.ContentNotFoundException;
+import com.weblog.diary.model.DayBook;
+import com.weblog.diary.repo.DayBookRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class DaybookService {
+
+    private final DayBookRepo dayBookRepo;
+
+    @Autowired
+    public DaybookService(DayBookRepo dayBookRepo) {
+        this.dayBookRepo = dayBookRepo;
+    }
+
+    public DayBook addContent(DayBook dayBook) {
+        dayBook.setEntryCode(UUID.randomUUID().toString());
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        dayBook.setTimestamp(String.valueOf(timestamp.getTime()));
+        return dayBookRepo.save(dayBook);
+    }
+
+    public List<DayBook> findAllContent() {
+        return dayBookRepo.findAll();
+    }
+
+    public DayBook updateContent(DayBook dayBook) {
+        return dayBookRepo.save(dayBook);
+    }
+
+    public DayBook findContentById(Long id) {
+        return dayBookRepo.findDayBookById(id).orElseThrow(() -> new ContentNotFoundException("Content by id " + id + " was not found"));
+    }
+
+    public void dropContent(Long id) {
+        dayBookRepo.deleteDayBookById(id);
+    }
 }

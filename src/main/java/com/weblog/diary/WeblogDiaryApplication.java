e8490b24bd4d6ff932f17a6a49bbf8a8cd167469
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-08-21 08:51:34 +0530

    init git repo

---
+++ src/main/java/com/weblog/diary/WeblogDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.weblog.diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblogDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeblogDiaryApplication.class, args);
+	}
+
+}

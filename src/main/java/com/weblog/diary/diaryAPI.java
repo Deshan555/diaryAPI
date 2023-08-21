bd1b763fb3ab19f608df75ee4e95716642f74a74
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-08-21 21:57:26 +0530

    add openapi custom definitions

--- src/main/java/com/weblog/diary/diaryAPI.java
+++ src/main/java/com/weblog/diary/diaryAPI.java
@@ -1,10 +1,28 @@
 package com.weblog.diary;
 
 import com.weblog.diary.model.DayBook;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "diaryAPI Project(Spring-Boot)",
+                version = "1.0.0",
+                description = "\uD83D\uDE80 This API allows you to manage your daybook entries, recording your thoughts, moods, and experiences. Keep track of your journey with ease! \uD83C\uDF1F",
+                contact = @Contact(
+                        name = "https://github.com/Deshan555",
+                        email = "deshanjayashanka84@gmail.com"
+                ),
+                license = @License(
+                        url = "https://github.com/Deshan555/diaryAPI"
+                )
+        )
+)
 public class diaryAPI {
     public diaryAPI() {
         DayBook dayBook = new DayBook();
